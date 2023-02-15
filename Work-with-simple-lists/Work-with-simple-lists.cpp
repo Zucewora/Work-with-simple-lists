02bfd2e7587ca8fdcdb5ccad3d08245d0e321dbd
Roman <125295347+Zucewora@users.noreply.github.com>
2023-02-15 22:22:43 +0300

    Update Work-with-simple-lists.cpp

--- Work-with-simple-lists/Work-with-simple-lists.cpp
+++ Work-with-simple-lists/Work-with-simple-lists.cpp
@@ -1,21 +1,13 @@
-﻿
-#include <cmath>
+﻿#include <cmath>
 #include <algorithm>
 #include <Windows.h>
 #include <iostream>
 #include <string>
 
-
-
 #pragma warning (disable :4996)
 using namespace std;
 
-#pragma warning (disable :4996)
-
-
-
 int proverka(int x) {
-
     if (x > 0) {
         return x;
     }
@@ -27,16 +19,12 @@ int proverka(int x) {
         }
         return x;
     }
-
 }
 int proverka_Menu(int x) {
-
     if (((x > 0) && (x <= 7)) || (x == 8)) {
         return x;
     }
     else {
-
-
         while (((x <= 0) || (x >= 7))) {
             if (x == 8) {
                 break;
@@ -45,21 +33,14 @@ int proverka_Menu(int x) {
             cout << "Введите пункт меню" << endl;
             cin >> x;
         }
-
         return x;
     }
-
 }
-
-
-
 
 void Menu() {
     //Карандаши (жёсткость (строка: HB, 2H,..), цвет, цена)
     cout << endl;
-    cout << endl;
     cout << "ПУНКТЫ МЕНЮ" << endl;
-
     cout << "1. Ввести структуру " << endl;
     cout << "2. Вывести структуру " << endl;
     cout << "3. Сортировка по цвету карандаша " << endl;
@@ -74,21 +55,13 @@ void Menu() {
 struct Pen {
     double price; // цена для карандаша
     char color[10]; //цвет карандаша 
-
-
 };
 struct Hardness {
     int nPens;
     string hardness; //HB B B2 жесткость карандаша 
     int count_of_pair;
     struct Pen Pens[100]; // количество карандашей
-
 };
-
-
-
-
-
 
 int main() {
     setlocale(0, "");
@@ -98,54 +71,41 @@ int main() {
     int Punkt_menu;
     struct Pen buff;
     struct Hardness* M = new struct Hardness[N];
-
-
+    
     do {
         cout << "Введите пункт меню" << endl;
         Menu();
 
         cin >> Punkt_menu;
         Punkt_menu = proverka_Menu(Punkt_menu);
-
+        
         switch (Punkt_menu) {
         case 1: {
-
             cout << "Введите количество типов твердости карандашей" << endl;
             cin >> N;
-
             N = proverka(N);
 
             delete[] M;
             M = new struct Hardness[N];
 
             for (int i = 0; i < N; i++) {
-
                 cout << "Введите твердость карандаша  " << endl;
                 cin >> M[i].hardness;
-
                 cout << "Введите количество карандашей для твердости ->" << M[i].hardness << endl;
                 cin >> M[i].nPens;
                 M[i].nPens = proverka(M[i].nPens);
-
-
+                
                 for (int j = 0; j < M[i].nPens; j++) {
                     cout << "Введите цвет для карандаша" << endl;
                     cin >> M[i].Pens[j].color;
-
                     cout << "Введите цену для карандаша -> " << M[i].Pens[j].color << endl;
                     cin >> M[i].Pens[j].price;
-
                     M[i].Pens[j].price = proverka(M[i].Pens[j].price);
-
-
                 }
             }
-
             break;
         }
         case 2: {
-
-
             for (int i = 0; i < N; i++) {
                 cout << "Жесткость - " << M[i].hardness << endl;
                 for (int j = 0; j < M[i].nPens; j++) {
@@ -160,40 +120,25 @@ int main() {
             //Сортировка по цвету карандаша
             for (int d = 0; d < N; d++) {
                 for (int i = 0; i < M[d].nPens - 1; i++) {
-
-                    //cout << "Здание - " << M[i].color << endl;
-
                     for (int j = i + 1; j < M[d].nPens; j++) {
-
                         strupr(M[d].Pens[i].color);
                         strupr(M[d].Pens[j].color);
                         if (strcmp(M[d].Pens[i].color, M[d].Pens[j].color) > 0) {
-
                             buff = M[d].Pens[i];
                             M[d].Pens[i] = M[d].Pens[j];
                             M[d].Pens[j] = buff;
                         }
-
                     }
                 }
             }
-
             break;
-
-
         }
         case 4: {
             cout << "4. Сортировка по цене карандаша " << endl;
             for (int d = 0; d < N; d++) {
-
                 for (int i = 0; i < M[d].nPens - 1; i++) {
-
-
-
                     for (int j = i + 1; j < M[d].nPens; j++) {
-
                         if (M[d].Pens[i].price > M[d].Pens[j].price) {
-
                             buff = M[d].Pens[i];
                             M[d].Pens[i] = M[d].Pens[j];
                             M[d].Pens[j] = buff;
@@ -205,27 +150,19 @@ int main() {
 
         }
         case 5: {
-
             //сортировка по двум признакам 
             // 
             for (int d = 0; d < N; d++) {
-
                 for (int i = 0; i < M[d].nPens - 1; i++) {
-
                     for (int j = i + 1; j < M[d].nPens; j++) {
-
                         if (M[d].Pens[i].price == M[d].Pens[j].price) {
-
                             strupr(M[d].Pens[i].color);
                             strupr(M[d].Pens[j].color);
-
                             if (strcmp(M[d].Pens[i].color, M[d].Pens[j].color) > 0) {
-
                                 buff = M[d].Pens[i];
                                 M[d].Pens[i] = M[d].Pens[j];
                                 M[d].Pens[j] = buff;
                             }
-
                         }
                         else
                         {
@@ -260,18 +197,15 @@ int main() {
             cout << "the_last_hardest_pen  ->" << M[index_of_last_elem].hardness << " ->>  ";
             cout << M[index_of_last_elem].Pens[X].color << " price -> " << M[index_of_last_elem].Pens[X].price << endl;
             break;
-
         }
         case 7: {
             for (int d = 0; d < N; d++) {
                 M[d].count_of_pair = 0;
                 cout << "Количество карандашей для жесткости " << M[d].hardness << " это " << M[d].nPens;
                 for (int i = 0; i < M[d].nPens - 1; i++) {
-
                     for (int j = i + 1; j < M[d].nPens; j++) {
                         strupr(M[d].Pens[i].color);
                         strupr(M[d].Pens[j].color);
-
                         if ((strcmp(M[d].Pens[i].color, M[d].Pens[j].color) > 0)) {
                             M[d].count_of_pair++;
                         }
@@ -280,7 +214,6 @@ int main() {
                                 M[d].count_of_pair++;
                             }
                             else {
-
                                 j = M[d].nPens;
                             }
                         }
@@ -293,6 +226,7 @@ int main() {
             break;
         }
 
+                // примеры для ввода:
               // 11B 10B 9B 8B 7B 6B 5B 4B 3B 2B B F H 2H 3H 4H 5H 6H 7H 8H 9H 10H 11H 
 
               // 3 HB 2 Красный 30 Зеленый 80 B 3 Синий 89 Зеленый 5 Красный 145 4B 4 Зеленый 87 Красный 40 Желтый 8 Синий 1
@@ -302,7 +236,7 @@ int main() {
 
         } //switch
 
-    } while (Punkt_menu != 404);
+    } while (Punkt_menu != 8);
 
 
     system("pause");
